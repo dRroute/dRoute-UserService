a754d2593f5d46455bfc404457cc01562b506b0c
cyboravi <cyboravidell@gmail.com>
2025-01-26 05:17:36 +0530

    first commit

---
+++ src/main/java/com/droute/userservice/DrouteUserServiceApplication.java
@@ -0,0 +1,25 @@
+package com.droute.userservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class DrouteUserServiceApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(DrouteUserServiceApplication.class);
+
+    public static void main(String[] args) {
+
+    	// Load environment variables from .env file
+    	Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(DrouteUserServiceApplication.class, args);
+        logger.info("Started DrouteUserServiceApplication successfully...");
+    }
+
+}
---
+++ src/test/java/com/droute/userservice/DrouteUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.droute.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrouteUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
