703cc450aac5bf633b88d92ee20db0e010648011
Anukrity Srivastava <anukritysrivastava@gmail.com>
2025-05-06 01:35:05 +0530

    Added-reset-password, get-driver apis

---
+++ src/main/java/com/droute/userservice/dto/request/ResetPasswordRequestDTO.java
@@ -0,0 +1,24 @@
+package com.droute.userservice.dto.request;
+
+
+public class ResetPasswordRequestDTO {
+    private String email;
+    private String newPassword;
+
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
